5e5355a9861707bc53e38a668a82cb085340d161
hadriman <hadriman@live.fr>
2023-08-06 15:22:39 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,13 +1,13 @@
-import klarflib
+import klarfio
 
 # to create an empty object
-klarf_file = klarflib.klarf()
+klarf_file = klarfio.klarf()
 
 # to create an empty object with a bunch of fields pre-populated with bogus data so that you have an actual klarf structure to work with
-klarf_file = klarflib.klarf(populate=True)
+klarf_file = klarfio.klarf(populate=True)
 
 # this is to load klarf data from a file
-klarf_file = klarflib.klarf("./testfile_base.klarf")
+klarf_file = klarfio.klarf("./testfile_base.klarf")
 
 # lets print some defect data contained in that klarf
 columns = klarf_file_ref.data['FileRecord_1.8']['LotRecord_aLot.String']["WaferRecord_FirstWaferId"]['DefectList']['Columns']
