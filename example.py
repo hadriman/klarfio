6471df123d1e9cab2886479018300539ae15dc63
hadriman <hadriman@live.fr>
2023-08-06 15:04:27 +0200

    Add files via upload
    Updated the example file

--- example.py
+++ example.py
@@ -9,5 +9,12 @@ klarf_file = klarflib.klarf(populate=True)
 # this is to load klarf data from a file
 klarf_file = klarflib.klarf("./testfile_base.klarf")
 
-#this to export klarf data to a file
+# lets print some defect data contained in that klarf
+columns = klarf_file_ref.data['FileRecord_1.8']['LotRecord_aLot.String']["WaferRecord_FirstWaferId"]['DefectList']['Columns']
+first_defect = klarf_file_ref.data['FileRecord_1.8']['LotRecord_aLot.String']["WaferRecord_FirstWaferId"]['DefectList']['Data'][0]
+print("Here is the data of the first defect :")
+for col,value in  zip(columns,first_defect ):
+    print(f'{col["Column"]} : {value}')
+
+#this is to export klarf data to a file
 klarf_file.export_klarf("./test_output.klarf")
